96126d136db36c3a0c4bda2516de57c81382f055
VarunKudvaBxA <Varun.Kudva@bounteous.com>
2026-03-09 10:40:28 +0530

    Update GlobalExceptionHandler.java

--- order-service/src/main/java/com/example/order_service/exception/GlobalExceptionHandler.java
+++ order-service/src/main/java/com/example/order_service/exception/GlobalExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.example.order_service.exception;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(InvalidOrderException.class)
-    public String handleInvalidOrder(InvalidOrderException ex) {
-        return ex.getMessage();
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleInvalidOrder(InvalidOrderException ex) {
+        return new ErrorResponse(
+                LocalDateTime.now(),
+                ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value()
+        );
     }
 }
